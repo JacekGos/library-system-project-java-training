a3eb4076ec60d822ea9d9117ac5b924c77b627b7
jacekg93 <jacekg93@gmail.com>
2021-06-04 08:07:27 +0200

    Added insert method

--- LibrarySystemProject/src/com/jacekg/SqlDataBaseConnection.java
+++ LibrarySystemProject/src/com/jacekg/SqlDataBaseConnection.java
@@ -7,10 +7,14 @@ import java.sql.Statement;
 
 public class SqlDataBaseConnection {
 
-    static String url = "jdbc:sqlserver://DESKTOP-2NG6J3P;databaseName=LibraryProject_v1;integratedSecurity=true";
+    static String url = "jdbc:sqlserver://DESKTOP-2NG6J3P;databaseName=LibraryProject_v2;integratedSecurity=true";
 
     static Connection connObj;
 
+    /**
+     * C - create - tworzy
+     *
+     */
     public static void sqlConnection(){
 
         try {
@@ -18,8 +22,31 @@ public class SqlDataBaseConnection {
             System.out.println("Connected to MS Sql Server");
         } catch (SQLException throwables) {
             System.out.println("Connection error");
+
             throwables.printStackTrace();
         }
+
+    }
+
+    public void insertSqlDb(){
+
+        String sql = "INSERT INTO Librarian (name, surname, login, password, account_type)"
+                + "VALUES ('Wojtek', 'Gos', 'wojtekg', 'a', 1)";
+
+        Statement statement = null;
+        try {
+            connObj = DriverManager.getConnection(url);
+            statement = connObj.createStatement();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        int rows = statement.executeUpdate(sql);
+
+        if (rows > 0){
+            System.out.println("Rows has to be inserted");
+        }
+
+        connObj.close();
 
     }
 
